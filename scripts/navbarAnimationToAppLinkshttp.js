5007f9f465a6e6c8c4f08d1258f004effab78995
Ali <mdumarali101@gmail.com>
2021-05-31 10:19:14 -0400

    No use ui navigation

---
+++ scripts/navbarAnimationToAppLinkshttp.js
@@ -0,0 +1,19 @@
+//grabbing elemnts in navbar
+// console.log('this is a log');
+// const buildNotes = "./notes.html";
+// let create = document.getElementById('listContainer-1');
+// let features = document.querySelector('#listContainer-3');
+
+// Settingup Double Click function
+// create.addEventListener("click", function onclickReturnFunction() {
+//     console.log('someone clicked here');
+//     if(status = 200) {
+//         return buildNotes;
+//     } else {
+//         window.alert('Erorr 404 not found')
+//     }
+// });
+// features.addEventListener("click", function onclickReturnFunctionfeatures() {
+//     console.log('someone clicked here also');
+//     create.innerHTML = `<a href="notes.html">Create</a>`;
+// });
