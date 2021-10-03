0c6b2b6d543f2ca3a413f06a20c45d11634bcd00
Ali <mdumarali101@gmail.com>
2021-10-03 10:55:36 +0530

    dunno the changes made lastly

--- scriptsNotes/app.js
+++ scriptsNotes/app.js
@@ -3,19 +3,25 @@ showNotes();
 
 //User adds a note it to localStorage
 let addBtn = document.getElementById("addBtn");
-addBtn.addEventListener("click", function(e) {
+addBtn.addEventListener("click", function (e) {
   let addTxt = document.getElementById("addTxt");
+  let addTitle = document.getElementById("addTitle");
   let notes = localStorage.getItem("notes");
   if (notes == null) {
-    notesObj = [];  
+    notesObj = [];
   } else {
     notesObj = JSON.parse(notes);
   }
-  notesObj.push(addTxt.value);
+  let myObj = {
+    title: addTitle.value,
+    text: addTxt.value
+  }
+  notesObj.push(myObj);
   localStorage.setItem("notes", JSON.stringify(notesObj));
   addTxt.value = "";
+  addTitle.value = "";
   console.log(notesObj);
-  showNotes(); //declared a function
+  showNotes();
 });
 
 
@@ -30,10 +36,10 @@ function showNotes() {
   let html = "";
   notesObj.forEach(function (element, index) {
     html += `
-        <div class="noteCard my-3 mx-3" style="width: 18rem; border-radius: 20px; border: 1px solid #dee2e6;">
+        <div class="noteCard my-3 mx-3">
             <div class="card-body">
-                <h5 class="card-title">Note ${index + 1}</h5>
-                <p class="card-text">${element}</p>
+                <h5 class="card-title">${element.title}</h5>
+                <p class="card-text">${element.text}</p>
                 <div class="btn-Container" style="border-radius: 15px;">
                     <button id="${index}" onclick="deleteNote(this.id)"class="btn btn-primary" style="border-radius: 15px;">Delete</button>
                 </div>
